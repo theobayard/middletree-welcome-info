905e4a879d0f43d0fb16b82989fd5369c5c3f1d5
theobayard <theobayarddevolo@gmail.com>
2021-04-28 12:02:55 -0500

    Adding greeting customization

--- src/Components/InfoPage/InfoPage.js
+++ src/Components/InfoPage/InfoPage.js
@@ -2,9 +2,13 @@ import InfoSection from './InfoSection'
 import './InfoPage.css'
 
 function InfoPage(props) {
+    let greeting = props.greeting ? 
+        props.greeting :
+        <h1>Welcome {props.audience}!</h1>
+
     return (
         <div className="page" id="infoPage">
-            <h1>Welcome {props.audience}!</h1>
+            {greeting}
             <div className="infoSections">
                 {props.sections.map((section) => (
                     <InfoSection {...section}/>
